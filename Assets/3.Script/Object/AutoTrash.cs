cbf1800e9e87551e1981399c2859546b9f56e884
kdi2344 <dani2344@sookmyung.ac.kr>
2023-06-22 17:45:00 +0900

    [feat] autoTrash

---
+++ Assets/3.Script/Object/AutoTrash.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoTrash : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ingredient"))
+        {
+            //없어지는 scale 조정느낌
+            Destroy(other.transform.parent.parent.gameObject);
+        }
+    }
+}
