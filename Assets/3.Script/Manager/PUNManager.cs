68278617446806f89053e85a40a6ab5981faf2c9
kdi2344 <dani2344@sookmyung.ac.kr>
2023-07-18 17:59:28 +0900

    [feat] 뭐,,,

--- Assets/3.Script/Manager/PUNManager.cs
+++ Assets/3.Script/Manager/PUNManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Photon.Pun;
 using Photon.Realtime;
 
@@ -9,6 +10,9 @@ public class PUNManager : MonoBehaviourPunCallbacks
     //서버 접속부터 Master Server -> Lobby -> Room
     private readonly string gameversion = "1";
     public ServerSettings setting = null;
+    [SerializeField] Button ServerBtn;
+
+    public InputField NickName_i; //가지고 있다가 플레이어가 생성되면 부여
 
     //플레이어 프리팹
     public GameObject playerPrefabs;
@@ -31,7 +35,51 @@ public class PUNManager : MonoBehaviourPunCallbacks
         //Master 서버에 연결
         PhotonNetwork.ConnectToMaster(setting.AppSettings.Server, setting.AppSettings.Port, "");
         Debug.Log("Connect to Master Server");
+        ServerBtn.onClick.AddListener(JoinRandomRoomORCreateRoom); //onClick 이벤트 넣기
     }
+    public void JoinRandomRoomORCreateRoom() //랜덤 룸에 들어가거나 없으면 만들기
+    {
+        if (NickName_i.text.Equals(string.Empty))
+        {
+            Debug.Log("Empty");
+            return;
+        }
+        string nickname = NickName_i.text;
+        Debug.Log($"{nickname} Start Random Matching");
+        PhotonNetwork.LocalPlayer.NickName = nickname;
+
+        RoomOptions option = new RoomOptions();
+
+        option.MaxPlayers = 2;
+        //실질적으로 사용할 커스텀 프로퍼티 객체 생성
+        //option.CustomRoomProperties = new ExitGames.Client.Photon.Hashtable()
+        //{
+        //    { "MaxTime", i_maxtime}
+        //};
+        ////로비에서 커스텀 프로퍼티 등록 -> 게임에서 필터링 가능
+        //option.CustomRoomPropertiesForLobby = new string[] { "MaxTime" };
+
+        //방 참가 시도하고 실패하면 생성해서 방에 참가해야함
+        PhotonNetwork.JoinRandomOrCreateRoom
+        (
+            expectedCustomRoomProperties: new ExitGames.Client.Photon.Hashtable() { { "MaxTime", option.CustomRoomProperties["MaxTime"] } },
+            expectedMaxPlayers: (byte)option.MaxPlayers,  //안될 수 있음 유의
+            roomOptions: option
+        );
+        //btn.onClick.RemoveAllListeners();
+        ServerBtn.transform.GetChild(0).GetComponent<Text>().text = "Cancel";
+        ServerBtn.onClick.AddListener(CancelMatching);
+    }
+    public void CancelMatching()
+    {
+        Debug.Log("Cancel Matching");
+        Debug.Log("Left Room");
+        PhotonNetwork.LeaveRoom();
+        ServerBtn.onClick.RemoveAllListeners();
+        ServerBtn.transform.GetChild(0).GetComponent<Text>().text = "Join";
+        ServerBtn.onClick.AddListener(JoinRandomRoomORCreateRoom);
+    }
+
     public void Disconnect()
     {
         PhotonNetwork.Disconnect(); //포톤 서버와 연결 끊기
@@ -63,7 +111,19 @@ public class PUNManager : MonoBehaviourPunCallbacks
     {
         base.OnJoinedRoom();
         Debug.Log("Joined Room");
-        PhotonNetwork.Instantiate(playerPrefabs.name, Vector3.zero, Quaternion.identity);
+        //PhotonNetwork.Instantiate(playerPrefabs.name, Vector3.zero, Quaternion.identity);
+        UpdatePlayer();
+    }
+
+    public void UpdatePlayer()
+    {
+        //UserCountText.text = $"{PhotonNetwork.CurrentRoom.PlayerCount} / {PhotonNetwork.CurrentRoom.MaxPlayers}";
+        //이벤트 -> 현재 방에 있는 Player의 수와 max 플레이어가 같다면 실질적인 플레이를 생성하고, 플레이를 가능하게 만듦
+        if (PhotonNetwork.CurrentRoom.PlayerCount == PhotonNetwork.CurrentRoom.MaxPlayers)
+        {
+            PhotonNetwork.Instantiate(playerPrefabs.name, Vector3.zero, Quaternion.identity);
+            //MatchPanel.SetActive(false);
+        }
     }
 
     #endregion
