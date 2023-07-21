b3abd8326a0949538e728023eead7f68152e59f0
kdi2344 <dani2344@sookmyung.ac.kr>
2023-07-21 13:49:39 +0900

    [feat] mask

--- Assets/3.Script/AboutPhoton/IngamePhtonManager.cs
+++ Assets/3.Script/AboutPhoton/IngamePhtonManager.cs
@@ -12,23 +12,31 @@ public class IngamePhtonManager : MonoBehaviourPunCallbacks
     private void Awake()
     {
         pv = GetComponent<PhotonView>();
-        
     }
     private void Start()
     {
         //플레이어 생성
-        Vector3 pos = spawnPoints[PhotonNetwork.CurrentRoom.PlayerCount - 1].position;
-        Quaternion rot = spawnPoints[PhotonNetwork.CurrentRoom.PlayerCount - 1].rotation;
-
-        GameObject playerTemp = PhotonNetwork.Instantiate("Player", pos, rot, 0);
-        CP = PhotonNetwork.LocalPlayer.CustomProperties;
-        int colorNum = (int)CP["Color"];
-        //Debug.Log("colornum " + colorNum);
-        //PhotonNetwork.CurrentRoom.Players[PhotonNetwork.CurrentRoom.PlayerCount - 1].SetCustomProperties(new Hashtable { { "Color", PhotonNetwork.CurrentRoom.PlayerCount } });
-        if (colorNum != -1)
+        if (!SoundManager.instance.isSingle)
         {
-            //Debug.Log("Color Custom Properties = " + colorNum);
-            playerTemp.GetComponent<PlayerController>().InitColor(colorNum);
+            CP = PhotonNetwork.LocalPlayer.CustomProperties;
+            int colorNum = (int)CP["Color"];
+            Vector3 pos = spawnPoints[colorNum].position;
+            Quaternion rot = spawnPoints[colorNum].rotation;
+
+            GameObject playerTemp = PhotonNetwork.Instantiate("Player", pos, rot, 0);
+            Debug.Log(playerTemp.name);
+            if (colorNum != -1)
+            {
+                playerTemp.GetComponent<PlayerController>().InitColor(colorNum);
+            }
+
+            PlayerController[] players = FindObjectsOfType<PlayerController>();
+            if (players.Length > PhotonNetwork.CurrentRoom.PlayerCount)
+            {
+                PhotonNetwork.Destroy(players[players.Length - 1].GetComponent<PhotonView>());
+            }
         }
+        
+        
     }
 }
